1b8711fc5199f3cd365115c0eb68c49534b0234f
tevoran <j.pfeiffer93@gmx.de>
2014-05-16 19:12:40 +0200

    first random stars (used with a seed)

--- main.c
+++ main.c
@@ -6,12 +6,34 @@
 void main()
 {
   //Setting up OPX
-  long seed=823123574;
+  long seed=123456789;
   opx_init(resolution_x,resolution_y,32,0);
 
   //creating content
-  opx_add_star(0,0,15000,700,255,255,255,0);
-  opx_add_star(5000,1500,15000,130,0,150,0,1);
+    //creating stars
+    int n=opx_random(seed)%20;
+    long new_seed=opx_random(seed);
+    float x,y,z,radius;
+    int r,g,b;
+    for(n;n>=0;n--)
+      {
+	x=opx_random(new_seed)%200-100;
+	  new_seed=opx_random(new_seed);
+	y=opx_random(new_seed)%250-125;
+	  new_seed=opx_random(new_seed);
+	z=opx_random(new_seed)%3000;
+	  new_seed=opx_random(new_seed);
+	radius=opx_random(new_seed)%250;
+	  new_seed=opx_random(new_seed);
+	r=opx_random(new_seed)%256;
+	  new_seed=opx_random(new_seed);
+	g=opx_random(new_seed)%256;
+	  new_seed=opx_random(new_seed);
+	b=opx_random(new_seed)%256;
+	  new_seed=opx_random(new_seed);
+	opx_add_star(x,y,z,radius,r,g,b,n);
+      }
+  
 
   //creating player
   struct opx_vector_float player;
@@ -31,7 +53,7 @@ void main()
 	  float FPS=(float)(float)1000/(float)(timer-ltimer);
 	  printf("FPS: %f\n",FPS);
 	ltimer=timer;
-        opx_render(player,anglexy,anglexz,resolution_x,resolution_y,1);
+        opx_render(player,anglexy,anglexz,resolution_x,resolution_y,0);
 	//player.z=player.z+0.001;
 	printf("z: %f\n",player.z);
 	//break;
